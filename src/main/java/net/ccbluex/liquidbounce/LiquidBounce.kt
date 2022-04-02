05f05060e0d5e85119c8303dcc3a602ddb46f0dc
mos <77763580+TheMosKau@users.noreply.github.com>
2022-04-02 16:35:52 +0700

    Update LiquidBounce.kt

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -1,6 +1,6 @@
 /*
- * LiquidBounce+ Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * UwU
+ * A free open source UwU injection hacked client for Minecraft using Minecraft Forge.
  * https://github.com/WYSI-Foundation/LiquidBouncePlus/
  */
 package net.ccbluex.liquidbounce
