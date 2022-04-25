978e47c4f87c65500c1f0828febdce90d59e338b
mos <77763580+TheMosKau@users.noreply.github.com>
2022-04-26 00:30:38 +0700

    Update LiquidBounce.kt

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -38,7 +38,7 @@ object LiquidBounce {
 
     // Client information
     const val CLIENT_NAME = "LiquidBounce++"
-    const val CLIENT_VERSION = "0.1"
+    const val CLIENT_VERSION = "0.2"
     const val CLIENT_CREATOR = "CCBlueX, WYSI-Foundation and TheMosKau"
     const val CLIENT_CLOUD = "https://wysi-foundation.github.io/LiquidCloud/LiquidBounce"
 
