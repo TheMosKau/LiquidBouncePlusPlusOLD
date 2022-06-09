876702eb96ebeaabbb1052c5e1438c9a47e308f9
yorik100 <yorik100@hotmail.com>
2022-06-09 15:15:31 +0200

    Possible range check fix

--- src/main/java/net/ccbluex/liquidbounce/utils/extensions/PlayerExtension.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/extensions/PlayerExtension.kt
@@ -16,20 +16,33 @@ import kotlin.math.sqrt
  * Allows to get the distance between the current entity and [entity] from the nearest corner of the bounding box
  */
 fun Entity.getDistanceToEntityBox(entity: Entity): Double {
-    val eyes = this.getPositionEyes(1F)
-    val pos = getNearestPointBB(eyes, entity.entityBoundingBox)
-    val xDist = abs(pos.xCoord - eyes.xCoord)
-    val yDist = abs(pos.yCoord - eyes.yCoord)
-    val zDist = abs(pos.zCoord - eyes.zCoord)
-    return sqrt(xDist.pow(2) + yDist.pow(2) + zDist.pow(2))
+    return sqrt(squaredBoxedDistanceTo(entity))
 }
 
-fun getNearestPointBB(eye: Vec3, box: AxisAlignedBB): Vec3 {
-    val origin = doubleArrayOf(eye.xCoord, eye.yCoord, eye.zCoord)
+fun Entity.squaredBoxedDistanceTo(entity: Entity): Double {
+    val eyes = entity.getCameraPosVec(1F)
+    val pos = getNearestPointBB(eyes, boundingBox)
+
+    val xDist = pos.x - eyes.x
+    val yDist = pos.y - eyes.y
+    val zDist = pos.z - eyes.z
+
+    return xDist * xDist + yDist * yDist + zDist * zDist
+}
+
+fun getNearestPointBB(eyes: Vec3d, box: Box): Vec3d {
+    val origin = doubleArrayOf(eyes.x, eyes.y, eyes.z)
     val destMins = doubleArrayOf(box.minX, box.minY, box.minZ)
     val destMaxs = doubleArrayOf(box.maxX, box.maxY, box.maxZ)
+
+    // It loops through every coordinate of the double arrays and picks the nearest point
     for (i in 0..2) {
-        if (origin[i] > destMaxs[i]) origin[i] = destMaxs[i] else if (origin[i] < destMins[i]) origin[i] = destMins[i]
+        if (origin[i] > destMaxs[i]) {
+            origin[i] = destMaxs[i]
+        } else if (origin[i] < destMins[i]) {
+            origin[i] = destMins[i]
+        }
     }
-    return Vec3(origin[0], origin[1], origin[2])
+
+    return Vec3d(origin[0], origin[1], origin[2])
 }
