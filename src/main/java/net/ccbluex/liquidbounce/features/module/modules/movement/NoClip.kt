9564499ea0f5bd5a931e8141835bbf721e788973
yorik100 <yorik100@hotmail.com>
2022-06-11 14:54:43 +0200

    NoClip finally compiles properly

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoClip.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoClip.kt
@@ -11,41 +11,45 @@ import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.value.FloatValue
 import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.event.StrafeEvent
+import kotlin.math.*
 
 @ModuleInfo(name = "NoClip", description = "Allows you to freely move through walls (A sandblock has to fall on your head).", category = ModuleCategory.MOVEMENT)
-class NoClip: Module() {
+class NoClip : Module() {
 
-    private var speed = FloatValue("Speed", 1 F, 0 F, 10 F)
 
+    private val speed = FloatValue("Speed", 0.32F, 0F, 10F)
+	
     override fun onDisable() {
         mc.thePlayer.noClip = false
-        mc.thePlayer.motionX = 0.0
+		mc.thePlayer.motionX = 0.0
         mc.thePlayer.motionY = 0.0
         mc.thePlayer.motionZ = 0.0
     }
-
+	
     @EventTarget
-    public void onMove(final MoveEvent event) {
-        mc.thePlayer.noClip = true
+    fun onStrafe(event: StrafeEvent) {
+		mc.thePlayer.noClip = true
+		mc.thePlayer.jumpMovementFactor = 0.0f
         val speed = speed.get()
-        // Get the direction yaw needed to set the speed using LiquidBounce MathUtils
         val yaw = MovementUtils.getDirectionRotation(mc.thePlayer!!.rotationYaw, mc.thePlayer!!.moveStrafing, mc.thePlayer!!.moveForward).toDouble()
-        mc.thePlayer.onGround = false
-        // Check if the player is moving, if not then don't make the player move horizontally
-        if (mc.thePlayer!!.movementInput.moveForward != 0 F || mc.thePlayer!!.movementInput.moveStrafe != 0 F) {
-            event.setX(-sin(Math.toRadians(yaw)) * speed)
-            event.setZ(cos(Math.toRadians(yaw)) * speed)
-        } else {
-            event.zeroXZ()
-        }
-        // Check whether or not the player wants to go up or down, if both or none then don't make the player move vertically
-        if (mc.gameSettings.keyBindSneak.isKeyDown() && !mc.gameSettings.keyBindJump.isKeyDown()) {
-            event.setY(-speed)
-        } else if (!mc.gameSettings.keyBindSneak.isKeyDown() && mc.gameSettings.keyBindJump.isKeyDown()) {
-            event.setY(speed)
-        } else {
-            event.setY(0)
-        }
+		mc.thePlayer.onGround = false
+		if (mc.thePlayer!!.movementInput.moveForward != 0F || mc.thePlayer!!.movementInput.moveStrafe != 0F) {
+			mc.thePlayer!!.motionX = -Math.sin(Math.toRadians(yaw)) * speed.toDouble()
+			mc.thePlayer!!.motionZ = Math.cos(Math.toRadians(yaw)) * speed.toDouble()
+		} else {
+			mc.thePlayer!!.motionX = 0.0
+			mc.thePlayer!!.motionZ = 0.0
+		}
+		if (mc.gameSettings.keyBindSneak.isKeyDown() && !mc.gameSettings.keyBindJump.isKeyDown()) {
+			mc.thePlayer!!.motionY = -speed.toDouble()
+		}
+		else if (!mc.gameSettings.keyBindSneak.isKeyDown() && mc.gameSettings.keyBindJump.isKeyDown()) {
+			mc.thePlayer!!.motionY = speed.toDouble()
+		} else {
+			mc.thePlayer!!.motionY = 0.0
+		}
     }
 }
